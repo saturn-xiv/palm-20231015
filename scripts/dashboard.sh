0510bf1c4feb7e3c976422829a40db19d91597e5
Jeremy Zheng <jitang.zheng@gmail.com>
2021-09-19 15:27:22 -0700

    :tada: add vue packages

--- scripts/dashboard.sh
+++ scripts/dashboard.sh
@@ -5,13 +5,19 @@ set -e
 # https://create-react-app.dev/docs/getting-started
 # npx create-react-app dashboard --template typescript
 
-yarn add react-intl \
-    draft-js react-copy-to-clipboard react-dropzone react-quill \
-    google-map-react qrcode.react react-markdown react-helmet \
-    redux react-redux react-router-dom react-intl react-hook-form \
-    moment moment-timezone react-moment \
-    @mui/material@next @emotion/react @emotion/styled @mui/icons-material@next @fontsource/roboto \
-    js-cookie jwt-decode dinero.js
+# yarn add react-intl \
+#     draft-js react-copy-to-clipboard react-dropzone react-quill \
+#     google-map-react qrcode.react react-markdown react-helmet \
+#     redux react-redux react-router-dom react-intl react-hook-form \
+#     moment moment-timezone react-moment \
+#     @material-ui/core@next @emotion/react @emotion/styled @material-ui/icons@next @fontsource/roboto \
+#     grpc-web js-cookie jwt-decode dinero    
+
+# vue create dashboard
+yarn add vue-i18n vuex vue-router \
+    moment moment-timezone \
+    grpc-web js-cookie jwt-decode dinero.js@alpha \
+    vuetify
 
 echo 'done.'
 
