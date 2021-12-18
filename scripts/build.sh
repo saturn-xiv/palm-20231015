ffa56825aef2b3152728bd76ca947a7065059e3d
Jeremy Zheng <jitang.zheng@gmail.com>
2021-12-17 23:45:32 -0800

    :wrench: add ubuntu package

---
+++ scripts/build.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+set -e
+
+export WORKSPACE=$PWD
+export GIT_VERSION=$(git describe --tags --always --dirty --first-parent)
+export TARGET=$WORKSPACE/tmp/$GIT_VERSION
+
+build_ubuntu_backend() {
+    echo "build $1..."
+
+    sudo apt -y install libc6-dev:$1 libudev-dev:$1 \
+        libpq5:$1 libpq-dev:$1 libmysqlclient-dev:$1 libsqlite3-dev:$1
+
+    local root=$2/usr/bin
+    mkdir -p $root
+    cd $WORKSPACE
+    if [ "$1" = "amd64" ]
+    then
+        local target="x86_64-unknown-linux-gnu"
+        cargo build --target $target --release
+        cp target/$target/release/palm $root
+        strip -s $root/palm
+    elif [ "$1" = "armhf" ]
+    then
+        # PKG_CONFIG_ALLOW_CROSS=1
+        # PKG_CONFIG_DIR=
+        # PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig
+        # export PKG_CONFIG_ALLOW_CROSS PKG_CONFIG_DIR PKG_CONFIG_LIBDIR
+
+        local target="armv7-unknown-linux-gnueabihf"
+        cargo build --target $target --release
+        cp target/$target/release/palm $root
+        arm-linux-gnueabihf-strip -s $root/palm
+    elif [ "$1" = "arm64" ]
+    then
+        # PKG_CONFIG_ALLOW_CROSS=1
+        # PKG_CONFIG_DIR=
+        # PKG_CONFIG_LIBDIR=/usr/lib/aarch64-linux-gnu/pkgconfig
+        # export PKG_CONFIG_ALLOW_CROSS PKG_CONFIG_DIR PKG_CONFIG_LIBDIR
+
+        local target="aarch64-unknown-linux-gnu"
+        cargo build --target $target --release
+        cp target/$target/release/palm $root
+        aarch64-linux-gnu-strip -s $root/palm
+    else
+        echo "unknown arch $1"
+        exit 1
+    fi
+}
+
+build_dashboard(){
+    cd $WORKSPACE
+    if [ ! -d node_modules ]
+    then
+        yarn install
+    fi
+    cd $WORKSPACE/dashboard
+    if [ ! -d node_modules ]
+    then
+        yarn install
+    fi
+    # FIXME https://github.com/webpack/webpack/issues/14532
+    NODE_OPTIONS=--openssl-legacy-provider yarn build
+}
+
+build_deb(){
+    local target=$TARGET/$1/target
+    if [ -d $target ]
+    then
+        rm -rf $(dirname $target)
+    fi
+    mkdir -pv $target
+    cp -r $WORKSPACE/debian $target/
+    build_ubuntu_backend $1 $target
+
+    mkdir -pv $target/usr/share/palm
+    cp -av $WORKSPACE/dashboard/dist $target/usr/share/palm/dashboard
+    local -a packages=(
+        "bootstrap/dist"
+        "bulma/css"
+        "marked/marked.min.js"
+        "material-design-icons/iconfont"
+        "moment/dist"
+        "moment-timezone/builds/moment-timezone-with-data.min.js"
+        "@popperjs/core/dist"
+        "mdb-ui-kit/css"
+        "mdb-ui-kit/js"
+        "mdb-ui-kit/img"
+    )
+    for i in "${packages[@]}"
+    do
+        local p=node_modules/$i
+        local t=$(dirname "$target/usr/share/palm/$p")
+        mkdir -p $t
+        cp -av $WORKSPACE/$p $t/
+    done
+    
+    mkdir -pv $target/var/lib/palm
+    mkdir -pv $target/lib/systemd/system/
+    
+    mkdir -pv $target/etc/palm
+    cp -r $WORKSPACE/LICENSE $WORKSPACE/README.md $WORKSPACE/package.json $target/etc/palm/
+    echo "$GIT_VERSION $(date -R)" > $target/etc/palm/VERSION
+
+    if [ "$1" = "armhf" ]
+    then
+        CC=arm-linux-gnueabihf-gcc
+        CXX=arm-linux-gnueabihf-g++
+        export CC CXX
+    elif [ "$1" = "arm64" ]
+    then
+        CC=aarch64-linux-gnu-gcc
+        CXX=aarch64-linux-gnu-g++
+        export CC CXX
+    elif [ "$1" = "amd64" ]
+    then
+        CC=gcc
+        CXX=g++
+        export CC CXX
+    else
+        echo "unknown arch $1"
+        return 1
+    fi
+    
+    cd $target
+    dpkg-buildpackage -us -uc -b --host-arch $1
+}
+
+# -----------------------------------------------------------------------------
+
+export OS_NAME=$(lsb_release -is)
+export OS_CODE=$(lsb_release -cs)
+
+if [[ $OS_NAME == "Ubuntu" ]]
+then
+    build_dashboard
+    
+    build_deb amd64
+    build_deb arm64
+    build_deb armhf
+    
+elif [[ $OS_NAME == "Arch" ]]
+then
+    build_dashboard
+    cargo build --release
+else
+    echo "Unknowk os $OS_NAME"
+    exit 1
+fi
+
+
+echo 'done.'
+exit 0
