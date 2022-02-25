5cc99e733253670e448c6f32a37f6d7346baf902
Jeremy Zheng <jitang.zheng@gmail.com>
2022-02-25 07:08:05 -0800

    ::wrench: remove sudo

--- docker/jvm/start.sh
+++ docker/jvm/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-NAME=palm-jvm
+export NAME="palm-jvm-$USER"
 
 if podman container exists $NAME
 then
-    podman start -i -a --events-backend=file $NAME
+    podman start -i -a $NAME
 else
-    podman run --name $NAME -it --userns=keep-id --hostname=palm --user=$(id -ur):$(id -gr) --network host --events-backend=file -v $PWD:/workspace:z palm-jvm
+    podman run --name $NAME -it --hostname=palm --network host -v $PWD:/workspace:z palm-jvm
 fi
