a213b6d7dfc706ec76cc8c58f08c32c7c054dc64
Jeremy Zheng <jitang.zheng@gmail.com>
2022-04-01 13:57:12 -0700

    :construction: add migration support

--- include/palm/orm.hpp
+++ include/palm/orm.hpp
@@ -38,6 +38,152 @@ std::shared_ptr<soci::session> open(
 
 }  // namespace sqlite3
 
+namespace orm {
+class Logger : public soci::logger_impl {
+ public:
+  virtual void start_query(std::string const& query) {
+    BOOST_LOG_TRIVIAL(debug) << query;
+  }
+
+ private:
+  virtual soci::logger_impl* do_clone() const { return new Logger(); }
+};
+
+class Query {
+ public:
+  Query(Query const&) = delete;
+  void operator=(Query const&) = delete;
+
+  static Query& instance() {
+    static Query it;
+    return it;
+  }
+  inline std::string get(const std::string& key) const {
+    for (const auto& [_, tree] : this->trees) {
+      const auto it = tree.get_optional<std::string>(key);
+      if (it) {
+        return it.value();
+      }
+    }
+    {
+      std::stringstream ss;
+      ss << "can't find query " << key;
+      throw std::runtime_error(ss.str());
+    }
+  }
+
+  void load(const std::filesystem::path& file);
+
+ private:
+  Query() {}
+  std::mutex locker;
+  std::unordered_map<std::string, boost::property_tree::ptree> trees;
+};
+
+struct Migration {
+  friend std::ostream& operator<<(std::ostream& out, const Migration& it) {
+    out << it.version << "\t" << it.name;
+    return out;
+  }
+
+  std::string up;
+  std::string down;
+  // LANG=C date +%Y%m%d%H%M%S
+  std::string version;
+  std::string name;
+  boost::optional<std::tm> run_on;
+  std::tm created_at;
+};
+class Schema {
+ public:
+  Schema(const std::filesystem::path& root, std::shared_ptr<soci::session> db);
+  void migrate();
+  void rollback();
+  void status(std::ostream& out);
+
+ private:
+  std::vector<Migration> migrations;
+  std::shared_ptr<soci::session> db;
+};
+
+class Factory {
+ public:
+  virtual std::shared_ptr<soci::session> create() const = 0;
+  virtual size_t size() const = 0;
+};
+
+class Pool {
+ public:
+  Pool(Pool const&) = delete;
+  void operator=(Pool const&) = delete;
+
+  static Pool& instance() {
+    static Pool it;
+    return it;
+  }
+  inline std::shared_ptr<soci::session> borrow() {
+    const std::lock_guard<std::mutex> lock(this->locker);
+
+    if (this->pool.size() == 0) {
+      for (auto& it : this->borrowed) {
+        if (it.unique()) {
+          BOOST_LOG_TRIVIAL(warning)
+              << R"(creating new database connection to replace discarded connection)";
+          std::shared_ptr con = this->factory->create();
+          this->borrowed.erase(it);
+          this->borrowed.insert(con);
+          return con;
+        }
+      }
+
+      throw std::runtime_error("no available connection in database pool");
+    }
+
+    std::shared_ptr<soci::session> it = this->pool.front();
+    this->pool.pop_front();
+    this->borrowed.insert(it);
+    return it;
+  }
+
+  inline void release(std::shared_ptr<soci::session> it) {
+    const std::lock_guard<std::mutex> lock(this->locker);
+    this->pool.push_back(it);
+    this->borrowed.erase(it);
+  }
+
+  void open(std::shared_ptr<Factory> factory) {
+    const std::lock_guard<std::mutex> lock(this->locker);
+
+    while (this->pool.size() < factory->size()) {
+      this->pool.push_back(factory->create());
+    }
+    this->factory = factory;
+  }
+  size_t idle() {
+    const std::lock_guard<std::mutex> lock(this->locker);
+    return this->pool.size();
+  }
+
+ private:
+  Pool() {}
+
+  std::deque<std::shared_ptr<soci::session>> pool;
+  std::set<std::shared_ptr<soci::session>> borrowed;
+  std::shared_ptr<Factory> factory;
+  std::mutex locker;
+};
+
+class PooledConnection {
+ public:
+  PooledConnection() { this->db = Pool::instance().borrow(); }
+  ~PooledConnection() { Pool::instance().release(this->db); }
+  std::shared_ptr<soci::session> get() { return this->db; }
+
+ private:
+  std::shared_ptr<soci::session> db;
+};
+
+}  // namespace orm
 namespace mysql {
 /**
 use DB-NAME
@@ -46,6 +192,23 @@ desc TABLE-NAME;
 SELECT table_name FROM information_schema.tables WHERE table_schema =
 'databasename' AND table_name = 'testtable'; SHOW TABLES LIKE 'tablename';
 */
+
+class Factory : public palm::orm::Factory {
+ public:
+  Factory(const boost::property_tree::ptree& config);
+  std::shared_ptr<soci::session> create() const override;
+
+  inline size_t size() const override { return this->pool_size; }
+
+ private:
+  std::string host;
+  uint16_t port;
+  std::string database;
+  std::string user;
+  std::optional<std::string> password;
+  std::chrono::seconds timeout;
+  size_t pool_size;
+};
 std::shared_ptr<soci::session> open(
     const std::string& database, const std::string& host = "127.0.0.1",
     const uint32_t port = 3306, const std::string& user = "root",
@@ -59,23 +222,56 @@ https://www.postgresql.org/docs/current/runtime-config-logging.html
 /var/lib/postgres/data/postgresql.conf: log_statement = 'all'
 sudo journalctl -u postgresql -f
 */
+class Factory : public palm::orm::Factory {
+ public:
+  Factory(const boost::property_tree::ptree& config);
+  std::shared_ptr<soci::session> create() const override;
+
+  inline size_t size() const override { return this->pool_size; }
+
+ private:
+  std::string host;
+  uint16_t port;
+  std::string database;
+  std::string user;
+  std::optional<std::string> password;
+  std::chrono::seconds timeout;
+  size_t pool_size;
+};
 std::shared_ptr<soci::session> open(
     const std::string& database, const std::string& host = "127.0.0.1",
     const uint32_t port = 5432, const std::string& user = "postgres",
     const std::optional<std::string> password = std::nullopt,
     const std::chrono::seconds& timeout = std::chrono::seconds(5));
 }  // namespace postgresql
+}  // namespace palm
 
-namespace orm {
-class Logger : public soci::logger_impl {
- public:
-  virtual void start_query(std::string const& query) {
-    BOOST_LOG_TRIVIAL(debug) << query;
+namespace soci {
+template <>
+struct type_conversion<palm::orm::Migration> {
+  typedef soci::values base_type;
+
+  static void from_base(const soci::values& v, soci::indicator i,
+                        palm::orm::Migration& o) {
+    o.version = v.get<std::string>("version");
+    o.name = v.get<std::string>("name");
+    o.up = v.get<std::string>("up");
+    o.down = v.get<std::string>("down");
+    o.run_on = v.get<boost::optional<std::tm>>("run_on");
+    o.created_at = v.get<std::tm>("created_at");
   }
 
- private:
-  virtual soci::logger_impl* do_clone() const { return new Logger(); }
-};
-}  // namespace orm
+  static void to_base(const palm::orm::Migration& o, soci::values& v,
+                      soci::indicator& i) {
+    v.set("version", o.version);
+    v.set("name", o.name);
+    v.set("up", o.up);
+    v.set("down", o.down);
+    v.set("run_on", o.run_on,
+          o.run_on.is_initialized() ? soci::i_ok : soci::i_null);
+    v.set("created_at", o.created_at);
 
-}  // namespace palm
+    i = soci::i_ok;
+  }
+};
+}  // namespace soci
--- src/orm.cpp
+++ src/orm.cpp
@@ -74,3 +74,191 @@ std::shared_ptr<soci::session> palm::postgresql::open(
   it->set_logger(new palm::orm::Logger());
   return it;
 }
+
+palm::mysql::Factory::Factory(const boost::property_tree::ptree& config) {
+  this->host = config.get("mysql.host", "127.0.0.1");
+  this->port = config.get("mysql.port", 3306);
+  this->user = config.get("mysql.user", "root");
+  {
+    auto it = config.get_optional<std::string>("mysql.password");
+    if (it.has_value()) {
+      this->password = it.get();
+    }
+  }
+  this->database = config.get<std::string>("mysql.db-name");
+  {
+    size_t it = config.get("mysql.connection-timeout", 12);
+    this->timeout = std::chrono::seconds(it);
+  }
+  this->pool_size = config.get("mysql.pool-size", 32);
+}
+
+std::shared_ptr<soci::session> palm::mysql::Factory::create() const {
+  return palm::mysql::open(this->database, this->host, this->port, this->user,
+                           this->password, this->timeout);
+}
+
+palm::postgresql::Factory::Factory(const boost::property_tree::ptree& config) {
+  this->host = config.get("postgresql.host", "127.0.0.1");
+  this->port = config.get("postgresql.port", 5432);
+  this->user = config.get("postgresql.user", "postgres");
+  {
+    auto it = config.get_optional<std::string>("postgresql.password");
+    if (it.has_value()) {
+      this->password = it.get();
+    }
+  }
+  this->database = config.get<std::string>("postgresql.db-name");
+  {
+    size_t it = config.get("postgresql.connection-timeout", 12);
+    this->timeout = std::chrono::seconds(it);
+  }
+  this->pool_size = config.get("postgresql.pool-size", 32);
+}
+
+std::shared_ptr<soci::session> palm::postgresql::Factory::create() const {
+  return palm::postgresql::open(this->database, this->host, this->port,
+                                this->user, this->password, this->timeout);
+}
+
+void palm::orm::Query::load(const std::filesystem::path& root) {
+  const std::lock_guard<std::mutex> lock(this->locker);
+  for (const auto& it : std::filesystem::directory_iterator(root / "queries")) {
+    const std::string file = it.path().string();
+    BOOST_LOG_TRIVIAL(debug) << "load query file from " << file;
+    boost::property_tree::ptree tree;
+    boost::property_tree::read_ini(file, tree);
+    this->trees[file] = tree;
+  }
+}
+
+palm::orm::Schema::Schema(const std::filesystem::path& root,
+                          std::shared_ptr<soci::session> db)
+    : db(db) {
+  const auto top = root / "migrations";
+
+  {
+    std::ifstream fs;
+    std::ios_base::iostate mask = fs.exceptions() | std::ios::failbit;
+    fs.exceptions(mask);
+    fs.open(top / "initial-setup.sql");
+    std::string sql((std::istreambuf_iterator<char>(fs)),
+                    std::istreambuf_iterator<char>());
+    (*db) << sql;
+  }
+
+  const auto& query = palm::orm::Query::instance();
+  const std::string sql_by_version = query.get("schema-migrations.by-version");
+  const std::string sql_create = query.get("schema-migrations.create");
+  for (const auto& it : std::filesystem::directory_iterator(top)) {
+    const auto node = it.path();
+    if (!std::filesystem::is_directory(node)) {
+      continue;
+    }
+    BOOST_LOG_TRIVIAL(debug) << "load migration files from " << node;
+
+    Migration mig;
+    {
+      std::string fn = node.filename();
+      const size_t POS = 14;
+      mig.version = fn.substr(0, POS);
+      mig.name = fn.substr(POS + 1);
+    }
+
+    {
+      std::ifstream fs;
+      std::ios_base::iostate mask = fs.exceptions() | std::ios::failbit;
+      fs.exceptions(mask);
+      fs.open(node / "up.sql");
+      mig.up = std::string((std::istreambuf_iterator<char>(fs)),
+                           std::istreambuf_iterator<char>());
+    }
+    {
+      std::ifstream fs;
+      std::ios_base::iostate mask = fs.exceptions() | std::ios::failbit;
+      fs.exceptions(mask);
+      fs.open(node / "down.sql");
+      mig.down = std::string((std::istreambuf_iterator<char>(fs)),
+                             std::istreambuf_iterator<char>());
+    }
+
+    {
+      boost::optional<Migration> cur;
+
+      (*db) << sql_by_version, soci::use(mig), soci::into(cur);
+
+      if (cur.is_initialized()) {
+        if (cur->name != mig.name || cur->up != mig.up ||
+            cur->down != mig.down) {
+          std::stringstream ss;
+          ss << "bad migration record " << cur->version;
+          throw std::runtime_error(ss.str());
+        }
+      } else {
+        BOOST_LOG_TRIVIAL(warning) << "can't found, save it";
+        (*db) << sql_create, soci::use(mig.version, "version"),
+            soci::use(mig.name, "name"), soci::use(mig.up, "up"),
+            soci::use(mig.down, "down");
+      }
+    }
+  }
+}
+
+void palm::orm::Schema::migrate() {
+  const palm::orm::Query& query = palm::orm::Query::instance();
+  const std::string sql_all = query.get("schema-migrations.all");
+  const std::string sql_set_run_on = query.get("schema-migrations.migrate");
+  soci::rowset<palm::orm::Migration> rows = (this->db->prepare << sql_all);
+
+  for (const auto& it : rows) {
+    if (it.run_on) {
+      continue;
+    }
+    {
+      BOOST_LOG_TRIVIAL(info) << "run migration " << it;
+      soci::transaction tr(*this->db);
+      (*this->db) << it.up;
+      (*this->db) << sql_set_run_on, soci::use(it);
+      tr.commit();
+    }
+  }
+}
+
+void palm::orm::Schema::rollback() {
+  const palm::orm::Query& query = palm::orm::Query::instance();
+
+  boost::optional<Migration> cur;
+
+  (*this->db) << query.get("schema-migrations.latest"), soci::into(cur);
+  if (cur.is_initialized()) {
+    BOOST_LOG_TRIVIAL(info) << "rollback migration " << (*cur);
+    soci::transaction tr(*this->db);
+    (*this->db) << cur->down;
+    (*this->db) << query.get("schema-migrations.rollback"), soci::use(cur);
+    tr.commit();
+  } else {
+    BOOST_LOG_TRIVIAL(info) << "database is empty";
+  }
+}
+
+void palm::orm::Schema::status(std::ostream& out) {
+  const palm::orm::Query& query = palm::orm::Query::instance();
+  const std::string sql_all = query.get("schema-migrations.all");
+
+  soci::rowset<palm::orm::Migration> rows = (this->db->prepare << sql_all);
+  const auto flags = out.flags();
+  out << std::setiosflags(std::ios::left);
+  out << std::setw(14) << "VERSION"
+      << " " << std::setw(32) << "NAME"
+      << " "
+      << "RUN ON" << std::endl;
+  for (const auto& it : rows) {
+    out << it.version << " " << std::setw(32) << it.name << " ";
+    if (it.run_on.is_initialized()) {
+      out << std::asctime(&it.run_on.get());
+    } else {
+      out << "Pending" << std::endl;
+    }
+  }
+  out << std::setiosflags(flags);
+}
