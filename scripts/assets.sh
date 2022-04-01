e14971531d9da8ed774fbcf01989a8443a531ab8
Jeremy Zheng <jitang.zheng@gmail.com>
2022-04-01 06:59:39 -0700

    :bento: add third assets

---
+++ scripts/assets.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+yarn add marked moment moment-timezone d3 mermaid \
+    bootstrap @popperjs/core \
+    mdb-ui-kit \
+    bulma \
+    tailwindcss@latest \
+    @tabler/core \
+    admin-lte@^3.2 \
+    material-components-web material-design-icons @fontsource/roboto
+
+echo 'done.'
+exit 0
