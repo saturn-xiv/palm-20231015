d9d764238c2d4af5d26b9f1a74b7a872fca30b22
Jeremy Zheng <jitang.zheng@gmail.com>
2022-04-23 13:20:29 -0700

    :bug: remove ubuntu id

--- docker/jammy/build.sh
+++ docker/jammy/build.sh
@@ -5,7 +5,7 @@ set -e
 export VERSION=$(date "+%4Y%m%d%H%M%S")
 export CODE="palm-jammy"
 
-buildah pull ubuntu:focal
+buildah pull ubuntu:jammy
 buildah bud --layers -t $CODE .
 podman save -o $CODE-$VERSION.tar $CODE
 
