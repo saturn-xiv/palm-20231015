d2c3769edd5007426f303c4d0423c5dc033334b6
Jeremy Zheng <jitang.zheng@gmail.com>
2022-05-28 09:34:44 -0700

    :construction: add locales support

---
+++ dashboard/src/locales/en-US.ts
@@ -0,0 +1,4 @@
+import languages from '@/i18n/languages';
+import others from '@/i18n/en-US';
+
+export default { ...languages, ...others };
---
+++ dashboard/src/locales/zh-Hans.ts
@@ -0,0 +1,4 @@
+import languages from '@/i18n/languages';
+import others from '@/i18n/zh-Hans';
+
+export default { ...languages, ...others };
