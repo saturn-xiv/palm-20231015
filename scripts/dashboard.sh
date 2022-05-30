1fdcc9fdb4f176c061befc1531e9d9ab6bb46611
Jeremy Zheng <jitang.zheng@gmail.com>
2022-05-30 07:20:20 -0700

    :heavy_plus_sign: add @types/js-cookie

--- scripts/dashboard.sh
+++ scripts/dashboard.sh
@@ -4,7 +4,8 @@ set -e
 
 yarn add dayjs grpc-web \
     google-protobuf @types/google-protobuf \
-    js-cookie jwt-decode dinero.js@alpha video.js \
+    js-cookie @types/js-cookie \
+    jwt-decode dinero.js@alpha video.js \
     react-copy-to-clipboard react-quill@beta react-dropzone \
     google-map-react qrcode.react react-markdown \
     react-color @types/react-color \
