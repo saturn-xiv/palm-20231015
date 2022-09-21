e0b2b703cf526e66eacbc67c946c5f999c446218
Jeremy Zheng <jitang.zheng@gmail.com>
2022-09-21 06:25:21 +0530

    :construction: merge nut protocols

--- palm/src/lib.rs
+++ palm/src/lib.rs
@@ -1,5 +1,10 @@
 pub mod schema;
 
+#[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+pub mod v1 {
+    tonic::include_proto!("palm.v1");
+}
+
 pub mod auth {
     #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
     pub mod v1 {
