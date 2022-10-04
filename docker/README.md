97fabf2c85441e129f787ca22d32d6fa4f09f0dc
Jeremy Zheng <jitang.zheng@gmail.com>
2022-10-04 10:28:09 +0530

    :construction: add storage by minio

--- docker/README.md
+++ docker/README.md
@@ -10,5 +10,5 @@ sudo apt install crun podman buildah
   podman system reset # clean
   podman images # show images
   podman ps -a # show containers
-  podman load -i tmp/palm-CODE-TIMESTAMP.tar
+  podman load -i tmp/palm-CODE-TIMESTAMP.tar # import image
   ```
--- nut/src/controllers/attachments.rs
+++ nut/src/controllers/attachments.rs
@@ -1,10 +1,16 @@
+use std::io::{Seek, SeekFrom, Write};
 use std::ops::{Deref, DerefMut};
 
 use actix_multipart::Multipart;
 use actix_web::{post, web, HttpResponse, Responder, Result as WebResult};
-use bytes::BytesMut;
 use futures_util::TryStreamExt;
-use palm::{crypto::Aes, nut::v1, orm::postgresql::Pool as DbPool, try_web};
+use palm::{
+    crypto::Aes,
+    nut::v1,
+    orm::postgresql::{Connection as Db, Pool as DbPool},
+    try_web, Result,
+};
+use tempfile::tempfile;
 
 use super::super::{
     models::{
@@ -19,7 +25,7 @@ pub async fn create(
     user: User,
     db: web::Data<DbPool>,
     aes: web::Data<Aes>,
-    mut payload: Multipart,
+    payload: Multipart,
 ) -> WebResult<impl Responder> {
     let mut db = try_web!(db.get())?;
     let db = db.deref_mut();
@@ -31,44 +37,42 @@ pub async fn create(
         try_web!(aws.create_bucket(&bucket).await)?;
     }
 
-    while let Some(ref mut field) = payload.try_next().await? {
+    try_web!(save(db, user.id, &aws, &bucket, payload).await)?;
+
+    Ok(HttpResponse::Ok())
+}
+
+async fn save(
+    db: &mut Db,
+    user: i32,
+    aws: &v1::MinioProfile,
+    bucket: &str,
+    mut payload: Multipart,
+) -> Result<()> {
+    while let Some(mut field) = payload.try_next().await? {
         let title = {
             let cd = field.content_disposition();
             let it = cd.get_filename().unwrap_or("unknown");
             it.to_string()
         };
         let name = Attachment::name(&title);
-
         let content_type = {
             let it = field.content_type();
             it.clone()
         };
 
-        // TODO multipart chunk uploads
-        let mut body = BytesMut::with_capacity(0);
-
+        let mut file = tempfile()?;
         while let Some(chunk) = field.try_next().await? {
-            body.extend(&chunk);
+            file.write_all(&chunk)?;
         }
-        let body = body.freeze();
-        let body = body.as_ref();
-        let size = body.len();
+        file.sync_all()?;
+        file.seek(SeekFrom::Start(0))?;
+        let size = file.metadata()?.len() as usize;
 
-        // FIXME
-        // try_web!(
-        //     aws.put_object(&bucket, &name, &content_type, body.to_vec(),)
-        //         .await
-        // )?;
-        try_web!(AttachmentDao::create(
-            db,
-            user.id,
-            &bucket,
-            &name,
-            &title,
-            &content_type,
-            size
-        ))?;
+        aws.put_object(bucket, &name, content_type.as_ref(), &mut file, size)
+            .await?;
+        AttachmentDao::create(db, user, bucket, &name, &title, &content_type, size)?;
     }
 
-    Ok(HttpResponse::Ok())
+    Ok(())
 }
