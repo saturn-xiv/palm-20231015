fa440e783e6542ded86b23e44af9ec90fd8abdd0
Jeremy Zheng <jitang.zheng@gmail.com>
2022-11-17 11:19:52 +0530

    :construction: merge protocols

---
+++ palm/build.rs
@@ -0,0 +1,42 @@
+use std::env;
+use std::fs::{read_dir, File};
+use std::io::Write;
+use std::path::Path;
+use std::process::Command;
+
+fn shell(cmd: &mut Command) -> String {
+    String::from_utf8(cmd.output().unwrap().stdout)
+        .unwrap()
+        .trim()
+        .to_string()
+}
+
+fn main() {
+    for it in read_dir("protocols").unwrap() {
+        let it = it.unwrap();
+        let it = it.path();
+        if let Some(ext) = it.extension() {
+            if ext == "proto" {
+                tonic_build::compile_protos(&it.display().to_string()).unwrap();
+            }
+        }
+    }
+    {
+        let out_dir = env::var("OUT_DIR").unwrap();
+        let git_version = shell(
+            Command::new("git")
+                .arg("describe")
+                .arg("--tags")
+                .arg("--always")
+                .arg("--first-parent")
+                .arg("--dirty"),
+        );
+        let build_time = shell(Command::new("date").arg("-R"));
+
+        let dest_path = Path::new(&out_dir).join("env.rs");
+        let mut fd = File::create(&dest_path).unwrap();
+
+        writeln!(fd, r#"pub const GIT_VERSION: &str = "{}";"#, git_version).unwrap();
+        writeln!(fd, r#"pub const BUILD_TIME: &str = "{}";"#, build_time).unwrap();
+    }
+}
--- palm/src/lib.rs
+++ palm/src/lib.rs
@@ -1,14 +1,100 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub const NAME: &str = env!("CARGO_PKG_NAME");
+pub const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+pub const HOMEPAGE: &str = env!("CARGO_PKG_HOMEPAGE");
+pub const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
+pub const BANNER: &str = include_str!("banner.txt");
+
+include!(concat!(env!("OUT_DIR"), "/env.rs"));
+
+pub mod nut {
+    #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+    pub mod v1 {
+        tonic::include_proto!("palm.nut.v1");
+    }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub mod cms {
+    #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+    pub mod v1 {
+        tonic::include_proto!("palm.cms.v1");
+    }
+}
+pub mod forum {
+    #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+    pub mod v1 {
+        tonic::include_proto!("palm.forum.v1");
+    }
+}
+pub mod courses {
+    #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+    pub mod v1 {
+        tonic::include_proto!("palm.courses.v1");
+    }
+}
+pub mod mall {
+    #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+    pub mod v1 {
+        tonic::include_proto!("palm.mall.v1");
+    }
+}
+pub mod chats {
+    #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+    pub mod v1 {
+        tonic::include_proto!("palm.chats.v1");
+    }
+}
+pub mod babel {
+    #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+    pub mod v1 {
+        tonic::include_proto!("palm.babel.v1");
+    }
+}
+pub mod cscd {
+    #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+    pub mod v1 {
+        tonic::include_proto!("palm.cscd.v1");
+    }
+}
+pub mod cbeta {
+    #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+    pub mod v1 {
+        tonic::include_proto!("palm.cbeta.v1");
+    }
+}
+pub mod orchid {
+    #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+    pub mod v1 {
+        tonic::include_proto!("palm.orchid.v1");
+    }
+}
+pub mod ops {
+    pub mod mail {
+        #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+        pub mod v1 {
+            tonic::include_proto!("palm.ops.mail.v1");
+        }
+    }
+    pub mod vpn {
+        #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+        pub mod v1 {
+            tonic::include_proto!("palm.ops.vpn.v1");
+        }
+    }
+    pub mod ddns {
+        #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+        pub mod v1 {
+            tonic::include_proto!("palm.ops.ddns.v1");
+        }
+    }
+    pub mod metrics {
+        #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+        pub mod v1 {
+            tonic::include_proto!("palm.ops.metrics.v1");
+        }
+    }
+    pub mod router {
+        #[allow(clippy::match_single_binding, clippy::derive_partial_eq_without_eq)]
+        pub mod v1 {
+            tonic::include_proto!("palm.ops.router.v1");
+        }
     }
 }
