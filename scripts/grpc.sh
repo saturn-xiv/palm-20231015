e474dda19b3a20f4ab72d89c1f7dbdf13f5ba8c1
Jeremy Zheng <jitang.zheng@gmail.com>
2022-11-17 10:23:36 +0530

    :hammer: merge nodejs & grpc script

---
+++ scripts/grpc.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+function build_grpc() {
+    # https://grpc.io/docs/languages/cpp/quickstart/
+    if [ -L $HOME/.local/bin/protoc ]
+    then
+        echo 'already exists!'
+        return
+    fi
+    if [ -d $HOME/downloads/grpc ]
+    then
+        cd $HOME/downloads/grpc
+        git checkout master
+        git pull        
+        git checkout $1
+        # fix unable to find current revision in submodule path
+        # git pull --recurse-submodules
+        git submodule update --init --recursive
+    else
+        git clone --recurse-submodules -b $1 https://github.com/grpc/grpc.git $HOME/downloads/grpc
+    fi
+   
+    if [ -d $HOME/build/grpc ]
+    then
+        rm -r $HOME/build/grpc
+    fi
+    mkdir -pv $HOME/build/grpc
+    cd $HOME/build/grpc
+    cmake -DCMAKE_BUILD_TYPE=Release \
+    -DgRPC_INSTALL=ON \
+    -DgRPC_SSL_PROVIDER=package \
+    -DgRPC_BUILD_TESTS=OFF \
+    -DCMAKE_INSTALL_PREFIX=$HOME/.local $HOME/downloads/grpc
+    make -j
+    make install
+}
+
+if [ "$#" -ne 1 ]
+then
+    echo "USAGE: $0 GRPC_VERSION"
+    exit 1
+fi
+
+build_grpc $1
+
+exit 0
