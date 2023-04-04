31b59d9b051d0bc1b7cf5bc069a039b3dd666c3c
Jeremy Zheng <jitang.zheng@gmail.com>
2023-04-04 15:04:56 +0530

    :tada: create wechat-pay project

---
+++ musa/src/main/java/com/github/saturn_xiv/palm/plugins/musa/MusaApplication.java
@@ -0,0 +1,13 @@
+package com.github.saturn_xiv.palm.plugins.musa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusaApplication.class, args);
+    }
+
+}
---
+++ musa/src/test/java/com/github/saturn_xiv/palm/plugins/musa/MusaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.saturn_xiv.palm.plugins.musa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
