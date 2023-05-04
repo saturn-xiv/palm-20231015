3c3609ece35700f2fd36df1c1bf7331e8e093d96
Jeremy Zheng <jitang.zheng@gmail.com>
2023-05-04 21:57:39 +0530

    :bug: fix build on arch

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -10,7 +10,7 @@ export THRIFT_FLAGS="-DBUILD_COMPILER=OFF -DWITH_OPENSSL=OFF -DBUILD_JAVA=OFF -D
 
 
 build_gnu() {
-    local target=$WORKSPACE/build/palm-arch-$3-$2
+    local target=$WORKSPACE/build/palm-arch-$1-$2
     mkdir -p $target
     cd $target
     
@@ -18,7 +18,8 @@ build_gnu() {
         -DBUILD_TESTING=OFF -DBoost_NO_WARN_NEW_VERSIONS=1 $THRIFT_FLAGS \
         -DCASBIN_BUILD_TEST=OFF -DCASBIN_BUILD_BENCHMARK=OFF -DCASBIN_BUILD_PYTHON_BINDINGS=OFF -DCASBIN_INSTALL=OFF \
         -DMAILIO_BUILD_EXAMPLES=OFF -DMAILIO_BUILD_DOCUMENTATION=OFF -DMAILIO_BUILD_SHARED_LIBRARY=OFF -DMAILIO_BUILD_TESTS=OFF \
-        -DVCPKG_HOST_TRIPLET=x64-linux -DVCPKG_TARGET_TRIPLET=$1 -DCMAKE_TOOLCHAIN_FILE=$HOME/local/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$WORKSPACE/toolchains/$3.cmake
+        -DVCPKG_HOST_TRIPLET=x64-linux -DVCPKG_TARGET_TRIPLET=$1-linux -DCMAKE_TOOLCHAIN_FILE=$HOME/local/vcpkg/scripts/buildsystems/vcpkg.cmake
+        # -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$WORKSPACE/toolchains/$3.cmake
     make
 }
 
@@ -41,8 +42,8 @@ fi
 
 # -----------------------------------------------------------------------------
 
-build_gnu x64-linux Debug x86_64
-build_gnu x64-linux Release x86_64
+build_gnu x86_64 Debug 
+build_gnu x86_64 Release
 
 build_musl coconut x86_64-linux-musl Debug x86_64
 build_musl coconut x86_64-linux-musl Release x86_64
