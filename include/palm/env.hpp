9fd9403f13c39e7a867ef57dd77ed0779ac9a232
Jeremy Zheng <jitang.zheng@gmail.com>
2023-05-04 11:07:51 +0530

    :construction: add rabbitmq support

--- include/palm/env.hpp
+++ include/palm/env.hpp
@@ -1,22 +1,39 @@
 #pragma once
 
+#include <bits/stdc++.h>
+#include <sys/utsname.h>
+#include <unistd.h>
 #include <algorithm>
 #include <chrono>
+#include <climits>
+#include <cstdint>
 #include <cstdlib>
 #include <ctime>
+#include <deque>
+#include <exception>
 #include <filesystem>
 #include <fstream>
+#include <functional>
 #include <iomanip>
 #include <iostream>
+#include <map>
 #include <memory>
+#include <mutex>
 #include <optional>
+#include <random>
 #include <ranges>
+#include <set>
 #include <sstream>
+#include <stdexcept>
 #include <string>
+#include <unordered_map>
+#include <utility>
+#include <variant>
 #include <vector>
 
 #include <google/protobuf/arena.h>
 #include <spdlog/spdlog.h>
+#include <nlohmann/json.hpp>
 
 #define TOML_EXCEPTIONS 1
 #include <toml++/toml.h>
@@ -30,4 +47,29 @@ inline static const std::string BASH_FOOTER = R"SHELL(
 echo 'done.'
 exit 0
 )SHELL";
+
+namespace http {
+namespace status {
+constexpr int BAD_REQUEST = 400;
+constexpr int INTERNAL_SERVER_ERROR = 500;
+constexpr int NOT_FOUND = 404;
+constexpr int FORBIDDEN = 403;
+constexpr int OK = 200;
+}  // namespace status
+
+namespace header {
+inline static const std::string AUTHORIZATION = "Authorization";
+inline static const std::string BEARER = "Bearer ";
+inline static const std::string CONTENT_TYPE = "Content-Type";
+}  // namespace header
+namespace content_type {
+inline static const std::string TEXT_HTML = "text/html; charset=utf-8";
+inline static const std::string TEXT_PLAIN = "text/plain; charset=utf-8";
+inline static const std::string APPLICATION_JSON = "application/json";
+inline static const std::string APPLICATION_OCTET_STREAM =
+    "application/octet-stream";
+inline static const std::string GRPC = "application/grpc";
+}  // namespace content_type
+
+}  // namespace http
 }  // namespace palm
--- tests/queue.cpp
+++ tests/queue.cpp
@@ -1,5 +1,72 @@
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("RabbitMQ", "[amqp]") { REQUIRE(1 * 1 == 1); }
+#include "palm/queue.hpp"
+
+#define PALM_EXCHANGE_NAME "ex-echo"
+#define PALM_QUEUE_NAME "qu-echo"
+#define PALM_CONTENT_TYPE "plain"
+
+class EchoHandler : public palm::queue::Handler {
+ public:
+  void execute(const std::string& id, const std::string& content_type,
+               const std::string& payload) override {
+    std::cout << "receive message [" << id << "]@(" << content_type
+              << "): " << payload << std::endl;
+  }
+};
+
+TEST_CASE("RabbitMQ", "[rabbitmq]") {
+  toml::table node;
+  node.insert("virtual-host", "testing");
+
+  palm::rabbitmq::Config cfg(node);
+
+  SECTION("producer") {
+    auto con = cfg.open();
+
+    con->declare_queue(PALM_QUEUE_NAME);
+    for (int i = 0; i < 10; i++) {
+      std::stringstream ids;
+      ids << "queue: hello " << PALM_QUEUE_NAME << " " << i;
+      const auto id = ids.str();
+      std::stringstream mss;
+      mss << "by producer - world " << i;
+      const auto msg = mss.str();
+      std::cout << "[" << id << "]: " << msg << std::endl;
+      con->produce(PALM_QUEUE_NAME, id, PALM_CONTENT_TYPE, msg);
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+  }
+
+  SECTION("publisher") {
+    auto con = cfg.open();
+
+    std::cout << "start publisher " << std::endl;
+    con->declare_queue(PALM_EXCHANGE_NAME, PALM_QUEUE_NAME);
+
+    for (int i = 0; i < 10; i++) {
+      std::stringstream ids;
+      ids << "exchange: hello " << PALM_EXCHANGE_NAME << "." << PALM_QUEUE_NAME
+          << " " << i;
+      const auto id = ids.str();
+      std::stringstream mss;
+      mss << "by publisher - world " << i;
+      const auto msg = mss.str();
+      std::cout << "[" << id << "]: " << msg << std::endl;
+      con->publish(PALM_EXCHANGE_NAME, id, PALM_CONTENT_TYPE, msg);
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+  }
+
+  SECTION("consumer") {
+    auto con = cfg.open();
+    con->declare_queue(PALM_QUEUE_NAME);
+
+    EchoHandler handler;
+    for (;;) {
+      con->consume(PALM_QUEUE_NAME, handler);
+    }
+  }
+}
 
 TEST_CASE("Mosquitto", "[mqtt]") { REQUIRE(1 * 1 == 1); }
