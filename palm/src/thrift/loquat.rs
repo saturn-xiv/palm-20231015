e2a87215fa4249a63d3ecd044e51fdcd2e84d326
Jeremy Zheng <jitang.zhen@gmail.com>
2023-05-12 16:12:35 +0000

    :construction: remove token from thrift

--- palm/src/thrift/loquat.rs
+++ palm/src/thrift/loquat.rs
@@ -20,19 +20,14 @@ impl Jwt for Thrift {
     fn sign(&self, subject: &str, audience: &str, ttl: Duration) -> Result<String> {
         let (i_prot, o_prot) = self.open(JWT)?;
         let mut client = JwtSyncClient::new(i_prot, o_prot);
-        let token = client.sign(
-            self.token.clone(),
-            subject.to_string(),
-            audience.to_string(),
-            ttl.num_seconds(),
-        )?;
+        let token = client.sign(subject.to_string(), audience.to_string(), ttl.num_seconds())?;
         Ok(token)
     }
 
     fn verify(&self, token: &str, audience: &str) -> Result<String> {
         let (i_prot, o_prot) = self.open(JWT)?;
         let mut client = JwtSyncClient::new(i_prot, o_prot);
-        let subject = client.verify(self.token.clone(), token.to_string(), audience.to_string())?;
+        let subject = client.verify(token.to_string(), audience.to_string())?;
         Ok(subject)
     }
 }
@@ -41,15 +36,13 @@ impl Password for Thrift {
     fn sign(&self, plain: &[u8]) -> Result<Vec<u8>> {
         let (i_prot, o_prot) = self.open(HMAC)?;
         let mut client = HmacSyncClient::new(i_prot, o_prot);
-        let token = client.sign(self.token.clone(), plain.to_vec())?;
+        let token = client.sign(plain.to_vec())?;
         Ok(token)
     }
     fn verify(&self, code: &[u8], plain: &[u8]) -> bool {
         if let Ok((i_prot, o_prot)) = self.open(HMAC) {
             let mut client = HmacSyncClient::new(i_prot, o_prot);
-            return client
-                .verify(self.token.clone(), code.to_vec(), plain.to_vec())
-                .is_ok();
+            return client.verify(code.to_vec(), plain.to_vec()).is_ok();
         }
         false
     }
@@ -59,13 +52,13 @@ impl Secret for Thrift {
     fn encrypt(&self, plain: &[u8]) -> Result<(Vec<u8>, Vec<u8>)> {
         let (i_prot, o_prot) = self.open(AES)?;
         let mut client = AesSyncClient::new(i_prot, o_prot);
-        let token = client.encrypt(self.token.clone(), plain.to_vec())?;
+        let token = client.encrypt(plain.to_vec())?;
         Ok((token, Vec::new()))
     }
     fn decrypt(&self, code: &[u8], _iv: &[u8]) -> Result<Vec<u8>> {
         let (i_prot, o_prot) = self.open(AES)?;
         let mut client = AesSyncClient::new(i_prot, o_prot);
-        let subject = client.decrypt(self.token.clone(), code.to_vec())?;
+        let subject = client.decrypt(code.to_vec())?;
         Ok(subject)
     }
 }
@@ -78,7 +71,7 @@ impl Health for Thrift {
     fn check(&self) -> Result<()> {
         let (i_prot, o_prot) = self.open(HEALTH)?;
         let mut client = HealthSyncClient::new(i_prot, o_prot);
-        client.check(self.token.clone())?;
+        client.check()?;
         Ok(())
     }
 }
--- palm/src/thrift/mod.rs
+++ palm/src/thrift/mod.rs
@@ -14,7 +14,6 @@ use super::Result;
 pub struct Thrift {
     pub host: String,
     pub port: u16,
-    pub token: String,
 }
 
 impl Default for Thrift {
@@ -22,7 +21,6 @@ impl Default for Thrift {
         Self {
             host: "127.0.0.1".to_string(),
             port: 8080,
-            token: "change-me".to_string(),
         }
     }
 }
