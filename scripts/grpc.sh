edb154bd3b9c848671e7e5489654d71cbb01cdac
Jeremy Zheng <jitang.zheng@gmail.com>
2023-05-29 13:43:23 +0000

    :wrench: match grpc with vcpkg

--- scripts/grpc.sh
+++ scripts/grpc.sh
@@ -27,6 +27,13 @@ function build_grpc() {
     else
         git clone --recurse-submodules -b $1 https://github.com/grpc/grpc.git $SOURCE_ROOT
     fi
+
+    cd $HOME/downloads/grpc/third_party/protobuf
+    git checkout main
+    git pull
+    git checkout $2
+    git submodule update --init --recursive
+
    
     if [ -d $BUILD_ROOT ]
     then
@@ -40,15 +47,18 @@ function build_grpc() {
     -DCMAKE_INSTALL_PREFIX=$INSTALL_ROOT $SOURCE_ROOT
     make -j $(nproc --ignore=2)
     make install
+
+    echo "done($1, $2)."
 }
 
-if [ "$#" -ne 1 ]
-then
-    echo "USAGE: $0 GRPC_VERSION"
-    exit 1
-fi
+# if [ "$#" -ne 2 ]
+# then
+#     echo "USAGE: $0 GRPC_VERSION PROTOBUF_VERSION"
+#     exit 1
+# fi
 
-build_grpc $1
+# vcpkg: 2023.04.15
+build_grpc "v1.51.1" "v3.21.12"
 
 
 if [[ $UBUNTU_CODENAME == "bionic" ]]
@@ -56,5 +66,4 @@ then
     cp $SOURCE_ROOT/third_party/re2/re2.pc $HOME/.local/lib/pkgconfig/
 fi
 
-echo "done($1)."
 exit 0
