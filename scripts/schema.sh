891f79cb19b682f5f6a8ce9f060648b45fb5386e
Jeremy Zheng <jitang.zhen@gmail.com>
2023-05-29 15:24:45 +0000

    :wrench: add generate thrift code

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -23,8 +23,32 @@ function generate_grpc_by_lang() {
         $WORKSPACE/$2
 }
 
+function generate_thrift_by_lang() {
+    echo "generate thrift($1) $2 => $3"
+    local target=$WORKSPACE/$3
+    
+    mkdir -p $target
+
+    thrift -out $3 --gen $1 -r $WORKSPACE/$2
+}
+
 # -----------------------------------------------------------------------------
+cd $WORKSPACE
+declare -a tmp_folders=(
+    "babel/protocols"
+    "loquat/gourd/src"
+)
+for t in "${tmp_folders[@]}"
+do
+    if [ -d $t ]
+    then 
+        rm -r $t
+    fi
+done
+
+
 generate_grpc_by_lang cpp protocols/babel.proto babel/protocols
+generate_thrift_by_lang cpp protocols/loquat.thrift loquat/gourd/src
 # -----------------------------------------------------------------------------
 
 echo 'done.'
