c6221cb9008748552abff3cd0c25e4c80cdc35e5
Jeremy Zheng <jitang.zheng@gmail.com>
2023-07-26 11:57:48 +0530

    :building_construction: add loading page

--- fig/dashboard/src/pages/loading.tsx
+++ fig/dashboard/src/pages/loading.tsx
@@ -1,7 +1,20 @@
 import RefreshIcon from "@mui/icons-material/Refresh";
+import Grid from "@mui/material/Unstable_Grid2";
 
 const Widget = () => {
-  return <RefreshIcon />;
+  return (
+    <Grid container spacing={2} minHeight={320}>
+      <Grid
+        display="flex"
+        mdOffset={4}
+        md={4}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <RefreshIcon fontSize="large" />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Widget;
