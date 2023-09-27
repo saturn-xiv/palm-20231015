308f52b2f842fba36964dee83fb6f30b19439ce4
Jeremy Zheng <jitang.zheng@gmail.com>
2023-09-27 17:54:36 +0800

    :tada: create gardenia project

---
+++ gardenia/src/main/java/com/github/saturn_xiv/palm/plugins/gardenia/GardeniaApplication.java
@@ -0,0 +1,13 @@
+package com.github.saturn_xiv.palm.plugins.gardenia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardeniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardeniaApplication.class, args);
+    }
+
+}
