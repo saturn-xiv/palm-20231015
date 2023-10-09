92b7605a648f894868c7fb3f18aa87839834cfad
Jeremy Zheng <jitang.zheng@gmail.com>
2023-10-09 08:25:39 +0000

    :bug: fix exit bug

--- scripts/musl.sh
+++ scripts/musl.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+set -e
+
+apk update
 apk add rust cargo pkgconf \
     linux-lts-dev openssl-dev \
     libpq-dev mariadb-dev sqlite-dev \
     protobuf protobuf-dev grpc grpc-dev
 
 OPENSSL_NO_VENDOR=Y cargo build --release
+
+echo 'done.'
+exit 0
