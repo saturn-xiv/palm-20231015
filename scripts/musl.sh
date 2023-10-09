35f828e2ba087d8a956afed7fcc2b4146751c4cc
Jeremy Zheng <jitang.zheng@gmail.com>
2023-10-09 07:22:17 +0000

    :hammer: add build on alpine

---
+++ scripts/musl.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+apk add rust cargo pkgconf \
+    linux-lts-dev openssl-dev \
+    libpq-dev mariadb-dev sqlite-dev \
+    protobuf protobuf-dev grpc grpc-dev
+
+OPENSSL_NO_VENDOR=Y cargo build --release
