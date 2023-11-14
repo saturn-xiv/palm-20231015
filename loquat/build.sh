fa76268e1e125a34ae9713713a157a49fd24005d
Jeremy Zheng <jitang.zheng@gmail.com>
2023-11-14 04:03:01 +0000

    :bug: build to ~/build

--- loquat/build.sh
+++ loquat/build.sh
@@ -11,9 +11,8 @@ then
     exit 1
 fi
 
-
-
-export WORKSPACE=$PWD/build/$(lsb_release -cs)-$(uname -m)-Release
+export SOURCE_DIR=$PWD
+export BUILD_DIR=$HOME/build/palm-loquat-$(lsb_release -cs)-$(uname -m)-Release
 export GCC_VERSION=12
 export THRIFT_FLAGS="-DBUILD_TESTING=OFF -DBUILD_COMPILER=OFF -DWITH_OPENSSL=ON -DBUILD_JAVA=OFF -DBUILD_JAVASCRIPT=OFF -DBUILD_NODEJS=OFF -DBUILD_PYTHON=OFF"
 export TINK_FLAGS="-DABSL_PROPAGATE_CXX_STD=ON -DTINK_USE_SYSTEM_OPENSSL=ON"
@@ -22,15 +21,13 @@ sudo apt install -y build-essential cmake pkg-config g++-$GCC_VERSION \
     golang libssl-dev libunwind-dev libboost-all-dev libevent-dev
 
 
-if [ ! -d $WORKSPACE ]
+if [ ! -d $BUILD_DIR ]
 then
-    mkdir -p $WORKSPACE
+    mkdir -p $BUILD_DIR
 fi
 
-cd $WORKSPACE
-
-CC=gcc-$GCC_VERSION CXX=g++-$GCC_VERSION cmake -DCMAKE_BUILD_TYPE=Release $THRIFT_FLAGS $TINK_FLAGS ../..
-make
+CC=gcc-$GCC_VERSION CXX=g++-$GCC_VERSION cmake -DCMAKE_BUILD_TYPE=Release $THRIFT_FLAGS $TINK_FLAGS -B $BUILD_DIR -S $SOURCE_DIR
+make -C $BUILD_DIR
 
 echo 'done.'
 
