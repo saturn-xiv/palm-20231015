323ade47394f620a1fb07442cfedfeb7fc1deaef
Jeremy Zheng <jitang.zheng@gmail.com>
2023-11-14 03:38:37 +0000

    :bug: fix loquat build on ubuntu

--- loquat/loquat/build.sh
+++ loquat/loquat/build.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+
+source /etc/os-release
+
+if [ $ID != "ubuntu" ]
+then
+    echo "unsupported system: $(lsb_release -ds)"
+    exit 1
+fi
+
+sudo apt install -y build-essential cmake pkg-config g++-12 \
+    golang libssl-dev libunwind-dev libboost-all-dev libevent-dev
+
 export WORKSPACE=$PWD/build/$(lsb_release -is)-$(uname -m)-Release
 
 export THRIFT_FLAGS="-DBUILD_TESTING=OFF -DBUILD_COMPILER=OFF -DWITH_OPENSSL=ON -DBUILD_JAVA=OFF -DBUILD_JAVASCRIPT=OFF -DBUILD_NODEJS=OFF -DBUILD_PYTHON=OFF"
@@ -11,8 +23,10 @@ if [ ! -d $WORKSPACE ]
 then
     mkdir -p $WORKSPACE
 fi
-  
-CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release $THRIFT_FLAGS $TINK_FLAGS ../..
+
+cd $WORKSPACE
+
+CC=gcc-12 CXX=g++-12 cmake -DCMAKE_BUILD_TYPE=Release $THRIFT_FLAGS $TINK_FLAGS ../..
 make loquat
 
 echo 'done.'
