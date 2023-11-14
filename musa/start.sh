37c76bdf033fd4d870537c5d3f3938812b3ed710
Jeremy Zheng <jitang.zheng@gmail.com>
2023-11-14 02:04:01 +0000

    :bug: fix build musa container

--- musa/start.sh
+++ musa/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+set -e
+
+if [ "$#" -ne 1 ]
+then
+    echo "USAGE: $0 VERSION"
+    exit 1
+fi
+
 export CODE="palm-musa"
 
-podman run --rm --events-backend=file --hostname=palm --network host $CODE /bin/bash -i -c "python . $*"
+podman run --rm --events-backend=file --hostname=palm --network host $CODE /usr/bin/java -jar musa-$1.jar
+
+exit 0
