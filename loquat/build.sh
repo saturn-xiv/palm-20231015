ee79bd3a1223a79a328401d6d2bb6e2427e03989
Jeremy Zheng <jitang.zheng@gmail.com>
2023-11-14 03:48:10 +0000

    :truck: move loquat to top folder

--- loquat/build.sh
+++ loquat/build.sh
@@ -2,14 +2,36 @@
 
 set -e
 
-export VERSION=$(date "+%4Y%m%d%H%M%S")
-export CODE="palm-loquat"
 
-buildah pull ubuntu:latest
-buildah bud --layers -t $CODE .
-podman save --format=oci-archive -o $CODE-$VERSION.tar $CODE
-md5sum $CODE-$VERSION.tar > $CODE-$VERSION.md5
+source /etc/os-release
 
-echo "done($CODE-$VERSION.tar)."
+if [ $ID != "ubuntu" ]
+then
+    echo "unsupported system: $(lsb_release -ds)"
+    exit 1
+fi
+
+
+
+export WORKSPACE=$PWD/build/$(lsb_release -cs)-$(uname -m)-Release
+export GCC_VERSION=12
+export THRIFT_FLAGS="-DBUILD_TESTING=OFF -DBUILD_COMPILER=OFF -DWITH_OPENSSL=ON -DBUILD_JAVA=OFF -DBUILD_JAVASCRIPT=OFF -DBUILD_NODEJS=OFF -DBUILD_PYTHON=OFF"
+export TINK_FLAGS="-DABSL_PROPAGATE_CXX_STD=ON -DTINK_USE_SYSTEM_OPENSSL=ON"
+
+sudo apt install -y build-essential cmake pkg-config g++-$GCC_VERSION \
+    golang libssl-dev libunwind-dev libboost-all-dev libevent-dev
+
+
+if [ ! -d $WORKSPACE ]
+then
+    mkdir -p $WORKSPACE
+fi
+
+cd $WORKSPACE
+
+CC=gcc-$GCC_VERSION CXX=g++-$GCC_VERSION cmake -DCMAKE_BUILD_TYPE=Release $THRIFT_FLAGS $TINK_FLAGS ../..
+make
+
+echo 'done.'
 
 exit 0
--- loquat/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export CODE="palm-loquat"
-
-podman run --rm --events-backend=file --hostname=palm --network host $CODE /bin/bash -i -c "python . $*"
