b41db345d932a1075c6d894f0f271a6ead385f53
Jeremy Zheng <jitang.zheng@gmail.com>
2023-11-18 07:40:35 +0000

    :construction: add desktop background

--- palm/palm/desktop/vite.config.ts
+++ palm/palm/desktop/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/desktop/",
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+});
---
+++ palm/palm/scripts/desktop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+npm install --save bulma
+
+echo 'done.'
+exit 0
