239420722ac08e8bec03395269ab834f8e7d0acd
Jeremy Zheng <jitang.zheng@gmail.com>
2023-11-19 09:49:34 +0000

    :construction: add health check support for morus

--- morus/morus/src/index.js
+++ morus/morus/src/index.js
@@ -2,6 +2,7 @@
 
 import { Server, ServerCredentials } from "@grpc/grpc-js";
 import { readFileSync } from "fs";
+import { HealthImplementation, ServingStatusMap } from "grpc-health-check";
 
 import { Config } from "./env";
 import logger from "./logger";
@@ -20,7 +21,7 @@ function main() {
     `load ca(${config.ca_file}) key(${config.key_file}) cert from(${config.cert_file})`
   );
 
-  const creds = ServerCredentials.createSsl(
+  const credentials = ServerCredentials.createSsl(
     readFileSync(config.ca_file),
     [
       {
@@ -31,10 +32,16 @@ function main() {
     true
   );
 
+  const statusMap = {
+    "": "SERVING",
+  };
+  const healthImpl = new HealthImplementation(statusMap);
+
   logger.info(`start gRPC server on tcps://0.0.0.0:${config.port}`);
   var server = new Server();
   server.addService(MarkdownService, { toHtml: to_html });
-  server.bindAsync(`0.0.0.0:${config.port}`, creds, () => {
+  healthImpl.addToServer(server);
+  server.bindAsync(`0.0.0.0:${config.port}`, credentials, () => {
     server.start();
   });
 }
--- morus/third.sh
+++ morus/third.sh
@@ -2,11 +2,17 @@
 
 set -e
 
-npm install --save @grpc/grpc-js google-protobuf \
+npm install --save @grpc/grpc-js grpc-health-check google-protobuf \
     marked dompurify jsdom canvas bufferutil utf-8-validate \
     pino pino-pretty
 
 npm install --save-dev webpack webpack-cli
+
+if [ -d health ]
+then
+    rm -r health
+fi
+cp -r node_modules/grpc-health-check/proto/health ./
 
 echo 'done.'
 exit 0
