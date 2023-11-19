7a2f95aac1aadca5e225345f765880ec9e89ee1a
Jeremy Zheng <jitang.zheng@gmail.com>
2023-11-19 07:51:45 +0000

    :construction: add tls support for morus

--- morus/morus/src/env.js
+++ morus/morus/src/env.js
@@ -1,11 +1,14 @@
 "use strict";
 
-import {readFileSync} from "fs"
+import { readFileSync } from "fs";
 
 export class Config {
-    constructor(file) {
-        const raw = readFileSync(file);
-        const it = JSON.parse(raw);
-        this.port = it.port;
-    }
+  constructor(file) {
+    const raw = readFileSync(file);
+    const it = JSON.parse(raw);
+    this.port = it.port;
+    this.ca_file = it["ca-file"];
+    this.key_file = it["key-file"];
+    this.cert_file = it["cert-file"];
+  }
 }
--- morus/morus/src/index.js
+++ morus/morus/src/index.js
@@ -1,25 +1,42 @@
 "use strict";
 
-import {Server, ServerCredentials} from '@grpc/grpc-js';
+import { Server, ServerCredentials } from "@grpc/grpc-js";
+import { readFileSync } from "fs";
 
-import {Config} from './env';
-import logger from './logger';
-import {MarkdownService} from './protocols/morus_grpc_pb';
-import { to_html } from './services/markdown';
+import { Config } from "./env";
+import logger from "./logger";
+import { MarkdownService } from "./protocols/morus_grpc_pb";
+import { to_html } from "./services/markdown";
 
 function main() {
-    const args = process.argv;
-    if(args.length !== 3){
-        logger.error(`USAGE: node ${args[1]} CONFIG_FILE`);
-        return;
-    }    
-    const config = new Config("config.json");
-    logger.info(`start gRPC server on http://0.0.0.0:${config.port}`);
-    var server = new Server();
-    server.addService(MarkdownService, {toHtml: to_html});
-    server.bindAsync(`0.0.0.0:${config.port}`, ServerCredentials.createInsecure(), ()=>{
-        server.start();
-    });
+  const args = process.argv;
+  if (args.length !== 3) {
+    logger.error(`USAGE: node ${args[1]} CONFIG_FILE`);
+    return;
+  }
+  const config = new Config("config.json");
+
+  logger.info(
+    `load ca(${config.ca_file}) key(${config.key_file}) cert from(${config.cert_file})`
+  );
+
+  const creds = ServerCredentials.createSsl(
+    readFileSync(config.ca_file),
+    [
+      {
+        private_key: readFileSync(config.key_file),
+        cert_chain: readFileSync(config.cert_file),
+      },
+    ],
+    true
+  );
+
+  logger.info(`start gRPC server on tcps://0.0.0.0:${config.port}`);
+  var server = new Server();
+  server.addService(MarkdownService, { toHtml: to_html });
+  server.bindAsync(`0.0.0.0:${config.port}`, creds, () => {
+    server.start();
+  });
 }
 
 main();
